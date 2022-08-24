37a178ffca4c008aacbc5cb4af72d6d4ef5a0707
Bruno Siqueira <104639923+brunosiq99@users.noreply.github.com>
2022-08-24 10:18:12 -0300

    1º commit

---
+++ assets/js/main.js
@@ -0,0 +1,128 @@
+//nav control
+const subjects = document.getElementsByName('subject');
+const navButton = document.querySelector('.nav__button');
+const subjectsContent = document.querySelectorAll('.subject')
+
+function chooseSubject(){
+    for(i= 0; i < subjects.length; i++){
+        if (subjects[i].checked){
+            subjects[i].parentNode.classList.add('header__nav___label-checked');
+            subjectsContent[i].style.display = 'block';  
+
+            
+        }else{
+            subjects[i].parentNode.classList.remove('header__nav___label-checked');
+            subjectsContent[i].style.display = 'none';    
+        }
+    } 
+}
+navButton.addEventListener('click', chooseSubject)
+
+
+//Load page
+
+loadPage();
+chooseSubject();
+
+//create the subject items on subject grid
+
+function loadPage(){
+    const apiUrlSubject = 'http://127.0.0.1:5500/assets/html/subject.html';
+    fetch(apiUrlSubject).
+        then((data) => data.json()).
+        then(response => 
+            response.forEach((listed) =>{
+                //create subject__item
+                const subject = listed.subject;
+                const section = document.querySelector(`[data-subject="${subject}"]`);
+                const card = document.createElement('div');
+                card.className = "subject__item";
+                    
+
+                //create subject__identification
+                const cardIdentification = document.createElement('div');
+                const cardIdentificationH3 = document.createElement('h3');
+                const cardIdentificationP =  document.createElement('p');
+                    
+                cardIdentificationH3.innerText = listed.h3;
+                cardIdentificationP.innerText = listed.p;
+
+                cardIdentification.className = "subject__identification";
+                cardIdentificationH3.className = "subject__identification___title";
+                cardIdentificationP.className = "subject__identification___subtitle";
+
+
+                //create subject__button
+                const cardButton = document.createElement('button');
+                    
+                cardButton.className = "subject__button";
+                cardButton.innerHTML = '<i class="fa-solid fa-xl fa-angle-down"></i>';
+                    
+                
+                //create subject__content
+                const cardContent = document.createElement('ul');
+                cardContent.className = "subject__content";
+                cardContent.setAttribute('data-matter',`${listed.subject}__${listed.matter}`);
+        
+
+                //appendChild
+                section.appendChild(card);
+
+                card.appendChild(cardIdentification);
+                cardIdentification.appendChild(cardIdentificationH3);
+                cardIdentification.appendChild(cardIdentificationP);
+
+                card.appendChild(cardButton);
+                card.appendChild(cardContent);
+            })
+        ).
+        then(()=>{
+            const subjectButtons = document.querySelectorAll('.subject__button')
+            subjectButtons.forEach((button)=>{
+                button.addEventListener('click', () => {controlCard(button)});
+            })
+        }).
+        then(()=>{
+            loadPlaylist();
+        })
+    
+}
+
+function loadPlaylist(){                //Called on loadPage();
+    const apiUrlSubjectMatter = 'http://127.0.0.1:5500/assets/html/subject-matter.html'; 
+    fetch(apiUrlSubjectMatter).
+        then((data)=>data.json()).
+        then((response) => {
+            response.forEach((listed)=>{
+                const contentLink = document.createElement('li');
+                contentLink.className = 'subject__content___li';
+                contentLink.innerHTML = `<a href=${listed.link}>${listed.name}</a>`;
+                const contentUl = document.querySelector(`[data-matter="${listed.subjectMatter}"]`);        //ul who will receive the li
+                
+                contentUl.appendChild(contentLink);
+            })
+        })
+}
+    
+
+//Control Card
+function controlCard(button){           //Called on loadPage();
+    const cardContent = button.nextElementSibling;
+    changeButtonAngle(button)
+    if (cardContent.style.display == ''){
+        cardContent.style.display = 'block';
+    }else{
+        cardContent.style.display = '';
+    }   
+}
+function changeButtonAngle(button){     //Called on controlCard()
+    const buttonAngle = button.childNodes[0];
+    
+    buttonAngle.classList.toggle('fa-angle-down')
+    buttonAngle.classList.toggle('fa-angle-up');
+}
+
+
+
+
+
